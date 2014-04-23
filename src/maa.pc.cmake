d1ceeae4e3d830a4867da560b8da3ad5fa7fa8fb
Brendan Le Foll <brendan.le.foll@intel.com>
2014-04-23 17:28:14 +0100

    cmake: make maa a proper dynamic library and install it correctly
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

---
+++ src/maa.pc.cmake
@@ -0,0 +1,11 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=${prefix}
+libdir=${exec_prefix}/lib@LIB_SUFFIX@
+includedir=${prefix}/include
+
+Name: maa
+Description: Low Level Skeleton Library for Communication
+Version: @maa_VERSION_STRING@
+
+Libs: -L${libdir} -lmaa
+Cflags: -I${includedir}/maa -lpthread
