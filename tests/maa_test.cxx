94941f51004cbd96c36ff4dad0ecbf7aa3b70f36
Brendan Le Foll <brendan.le.foll@intel.com>
2014-05-20 15:40:28 +0100

    gtest: fix version test and change test cmake var name
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

--- tests/maa_test.cxx
+++ tests/maa_test.cxx
@@ -1,8 +1,12 @@
-#include "maa.h"
+#include <maa.h>
 #include "gtest/gtest.h"
+#include "version.h"
 
-using namespace maa;
-
-TEST (basic, GetVersion) {
-    ASSERT_EQ(get_version(), 1);
+/* Careful, this test will only attempt to check the returned version is valid,
+ * it doesn't try to check the version is a release one.
+ */
+TEST (basic, maa_get_version) {
+    char bar[64];
+    strcpy(bar, maa_get_version());
+    ASSERT_STREQ(maa_get_version(), gVERSION);
 }
