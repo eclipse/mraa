8d21d477b8e940a082dc4cfafa91084abc2b8236
Thomas Ingleby <thomas.c.ingleby@intel.com>
2014-05-22 14:17:33 +0100

    examples: added more cpp examples.
    Mostly just the same functionality as the C examples yet using the cpp
    api.
    
    Signed-off-by: Thomas Ingleby <thomas.c.ingleby@intel.com>

---
+++ examples/c++/Pwm3-cycle.cpp
@@ -0,0 +1,65 @@
+/*
+ * Author: Thomas Ingleby <thomas.c.ingleby@intel.com>
+ * Copyright (c) 2014 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <unistd.h>
+#include <signal.h>
+
+#include "pwm.hpp"
+
+int running = 0;
+
+void
+sig_handler(int signo)
+{
+    if (signo == SIGINT) {
+        printf("closing PWM nicely\n");
+        running = -1;
+    }
+}
+
+int main ()
+{
+    maa::Pwm* pwm;
+
+    pwm = new maa::Pwm(3);
+    if (pwm == NULL) {
+        return MAA_ERROR_UNSPECIFIED;
+    }
+    fprintf(stdout, "Cycling PWM on IO3 (pwm3) \n");
+
+    signal(SIGINT, sig_handler);
+
+    float value = 0.0f;
+    while (running == 0) {
+        value = value + 0.01f;
+        pwm->write(value);
+        usleep(50000);
+        if (value >= 1.0f) {
+            value = 0.0f;
+        }
+    }
+    delete pwm;
+
+    return MAA_SUCCESS;
+}
---
+++ examples/c++/Spi-pot.cpp
@@ -0,0 +1,73 @@
+/*
+ * Author: Thomas Ingleby <thomas.c.ingleby@intel.com>
+ * Copyright (c) 2014 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <unistd.h>
+#include <signal.h>
+#include <stdint.h>
+
+#include "spi.hpp"
+
+int running = 0;
+
+void
+sig_handler(int signo)
+{
+    if (signo == SIGINT) {
+        printf("closing spi nicely\n");
+        running = -1;
+    }
+}
+
+int main ()
+{
+    maa::Spi* spi;
+
+    spi = new maa::Spi(0);
+
+    signal(SIGINT, sig_handler);
+
+    uint8_t data[] = {0x00, 100};
+    uint8_t *recv;
+    while (running == 0) {
+        int i;
+        for (i = 90; i < 130; i++) {
+            data[1] = i;
+            recv = spi->write_buf(data, 2);
+            printf("Writing -%i",i);
+            printf("RECIVED-%i-%i\n",recv[0],recv[1]);
+            usleep(100000);
+        }
+        for (i = 130; i > 90; i--) {
+            data[1] = i;
+            recv = spi->write_buf(data, 2);
+            printf("Writing -%i",i);
+            printf("RECIVED-%i-%i\n",recv[0],recv[1]);
+            usleep(100000);
+        }
+
+    }
+    delete spi;
+
+    return MAA_SUCCESS;
+}
