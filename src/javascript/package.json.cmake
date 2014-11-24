0ca4b10687715de0bd77fbf1035549e1c10d9ee6
Brendan Le Foll <brendan.le.foll@intel.com>
2014-11-24 17:45:50 +0000

    package.json.cmake: remove repository tag causing issues on update
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

--- src/javascript/package.json.cmake
+++ src/javascript/package.json.cmake
@@ -3,12 +3,6 @@
   "description": "IO library that helps you use I2c, SPI, gpio, uart, pwm, analog inputs (aio) and more on a number of platforms such as the Intel galileo, the Intel edison and others",
   "keywords":["gpio", "edison","galileo","io", "mraajs", "spi", "i2c", "minnow", "intel"],
   "homepage": "http://github.com/intel-iot-devkit/mraa",
-
-
-  "repository" : {
-    "type" : "git",
-    "url" : "http://github.com/intel-iot-devkit/mraa.git"
-  },
   "main" : "./mraa.node",
   "engines": {
     "node": ">= 1.0.x"
