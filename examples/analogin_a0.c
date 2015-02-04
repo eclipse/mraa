ac6badaf91aaec539553f7b300dd1d47d07203c1
Alex Tereschenko <alext.mkrs@gmail.com>
2015-02-04 20:09:56 +0100

    examples/aio: Added examples for mraa_aio_read_float()/readFloat()
    Signed-off-by: Alex Tereschenko <alext.mkrs@gmail.com>

--- examples/analogin_a0.c
+++ examples/analogin_a0.c
@@ -30,6 +30,7 @@ int main ()
 {
     mraa_aio_context adc_a0;
     uint16_t adc_value = 0;
+    float adc_value_float = 0.0;
 
     adc_a0 = mraa_aio_init(0);
     if (adc_a0 == NULL) {
@@ -38,7 +39,9 @@ int main ()
 
     for(;;) {
         adc_value = mraa_aio_read(adc_a0);
+        adc_value_float = mraa_aio_read_float(adc_a0);
         fprintf(stdout, "ADC A0 read %X - %d\n", adc_value, adc_value);
+        fprintf(stdout, "ADC A0 read float - %.5f\n", adc_value_float);
     }
 
     mraa_aio_close(adc_a0);
