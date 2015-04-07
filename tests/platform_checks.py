8cc957ac64a1bd1d9e54b3f2d0f9bb29854b5570
Costin Constantin <costin.c.constantin@intel.com>
2015-04-07 17:02:27 +0300

    tests: add python test cases suite
    Currently only galileo is supported. This commit removes the old GTEST stuff
    and mraa_test.cxx which never did much
    
    Signed-off-by: Costin Constantin <costin.c.constantin@intel.com>
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

---
+++ tests/platform_checks.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+# Author: Costin Constantin <costin.c.constantin@intel.com>
+# Copyright (c) 2015 Intel Corporation.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import unittest as u
+import mraa as m
+
+@u.skipIf(m.init() != m.ERROR_PLATFORM_ALREADY_INITIALISED, "mraa_init() not valid")
+class PlatformChecks(u.TestCase):
+  def test_mraa_check_platform_no_of_pins(self):
+    pinCount = m.getPinCount()
+    self.assertEqual(pinCount, 20, "Wrong number. of pins. Check platform ...")
+
+  def test_mraa_check_platform_ADC_max_resolution(self):
+    self.p_ADC_mres = m.adcRawBits()
+    print "Platform ADC max. resolution is: " + str(self.p_ADC_mres) + " bits"
+    self.assertEqual(self.p_ADC_mres, 12, "Wrong ADC max. resolution. Check platform ...")
+
+  def test_mraa_check_platform_ADC_resolution(self):
+    self.p_ADC_res = m.adcSupportedBits()
+    print "Platform ADC resolution is: " + str(self.p_ADC_res) + " bits"
+    self.assertEqual(self.p_ADC_res, 10, "Wrong ADC suported resolution. Check platform ...")
+
+if __name__ == "__main__":
+  u.main()
