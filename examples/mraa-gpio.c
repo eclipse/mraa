0b382100d2746bb659d5507f5c3b5a2bcaf4a8f6
Brendan Le Foll <brendan.le.foll@intel.com>
2015-04-24 16:05:12 +0100

    mraa-gpio.c: fix unused printf argument
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

--- examples/mraa-gpio.c
+++ examples/mraa-gpio.c
@@ -98,7 +98,7 @@ gpio_set(int pin, int level, mraa_boolean_t raw)
         if (raw != 0) {
             if (mraa_gpio_use_mmaped(gpio, 1) != MRAA_SUCCESS) {
                 fprintf(stdout,
-                        "mmapped access to gpio not supported, falling back to normal mode\n", pin);
+                        "mmapped access to gpio %d not supported, falling back to normal mode\n", pin);
             }
         }
         mraa_gpio_write(gpio, level);
