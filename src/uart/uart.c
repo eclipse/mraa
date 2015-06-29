fd50ea087b658d68fdd61921b8302f9a7f10242a
Thomas Ingleby <thomas.c.ingleby@intel.com>
2015-06-29 19:51:34 +0100

    uart: fix issue where a index is checked before set
    Related to #152
    
    Signed-off-by: Thomas Ingleby <thomas.c.ingleby@intel.com>

--- src/uart/uart.c
+++ src/uart/uart.c
@@ -142,8 +142,8 @@ mraa_uart_init(int index)
         }
     }
 
+    pos = plat->uart_dev[index].tx;
     if (pos >= 0) {
-        pos = plat->uart_dev[index].tx;
         if (plat->pins[pos].uart.mux_total > 0) {
             if (mraa_setup_mux_mapped(plat->pins[pos].uart) != MRAA_SUCCESS) {
                 syslog(LOG_ERR, "uart: failed to setup muxes for TX pin");
