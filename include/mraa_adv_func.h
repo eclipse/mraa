faa8b48e29fc543e286fb862bb57df5e9dcaa61b
Henry Bruce <henry.bruce@intel.com>
2015-07-27 09:39:15 -0700

    usb: Added gpio read/write support for ft4222 with i/o expander
    Signed-off-by: Henry Bruce <henry.bruce@intel.com>

--- include/mraa_adv_func.h
+++ include/mraa_adv_func.h
@@ -28,7 +28,12 @@
 #include "mraa.h"
 #include "types.h"
 
+// FIXME: Nasty macro to test for presence of function in context structure function table
+#define IS_FUNC_DEFINED(dev, func)   (dev != NULL && dev->advance_func != NULL && dev->advance_func->func != NULL)
+
+
 typedef struct {
+    mraa_result_t (*gpio_init_internal_replace) (int pin);
     mraa_result_t (*gpio_init_pre) (int pin);
     mraa_result_t (*gpio_init_post) (mraa_gpio_context dev);
 
@@ -37,11 +42,15 @@ typedef struct {
     mraa_result_t (*gpio_mode_replace) (mraa_gpio_context dev, mraa_gpio_mode_t mode);
     mraa_result_t (*gpio_mode_pre) (mraa_gpio_context dev, mraa_gpio_mode_t mode);
     mraa_result_t (*gpio_mode_post) (mraa_gpio_context dev, mraa_gpio_mode_t mode);
+    
+    mraa_result_t (*gpio_edge_mode_replace) (mraa_gpio_context dev, mraa_gpio_edge_t mode);
 
     mraa_result_t (*gpio_dir_replace) (mraa_gpio_context dev, mraa_gpio_dir_t dir);
     mraa_result_t (*gpio_dir_pre) (mraa_gpio_context dev, mraa_gpio_dir_t dir);
     mraa_result_t (*gpio_dir_post) (mraa_gpio_context dev, mraa_gpio_dir_t dir);
 
+    int (*gpio_read_replace) (mraa_gpio_context dev);
+    mraa_result_t (*gpio_write_replace) (mraa_gpio_context dev, int value);
     mraa_result_t (*gpio_write_pre) (mraa_gpio_context dev, int value);
     mraa_result_t (*gpio_write_post) (mraa_gpio_context dev, int value);
     mraa_result_t (*gpio_mmap_setup) (mraa_gpio_context dev, mraa_boolean_t en);
@@ -62,8 +71,6 @@ typedef struct {
     mraa_result_t (*i2c_write_byte_data_replace) (mraa_i2c_context dev, const uint8_t data, const uint8_t command);
     mraa_result_t (*i2c_write_word_data_replace) (mraa_i2c_context dev, const uint16_t data, const uint8_t command);
     mraa_result_t (*i2c_stop_replace) (mraa_i2c_context dev);
-
-
 
     mraa_result_t (*aio_get_valid_fp) (mraa_aio_context dev);
     mraa_result_t (*aio_init_pre) (unsigned int aio);
