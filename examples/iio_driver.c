8c19105309befb4c1f63a7ab58f007e0dc75d5d2
Brendan Le Foll <brendan.le.foll@intel.com>
2015-09-28 16:53:58 +0100

    iio_driver.c: Add example for iio interface
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

---
+++ examples/iio_driver.c
@@ -0,0 +1,49 @@
+/*
+ * Author: Brendan Le Foll
+ * Copyright (c) 2015 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <unistd.h>
+//! [Interesting]
+#include "mraa/iio.h"
+
+int
+main()
+{
+    mraa_iio_context iio_device0;
+
+    iio_device0 = mraa_iio_init(0);
+    if (iio_device0 == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    float iio_value;
+    mraa_result_t ret = mraa_iio_read(iio_device0, 0, "raw", &iio_value);
+    if (ret == MRAA_SUCCESS) {
+        fprintf(stdout, "IIO read %f\n", iio_value);
+    }
+
+    mraa_iio_stop(iio_device0);
+
+    return EXIT_SUCCESS;
+}
+//! [Interesting]
