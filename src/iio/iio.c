574d670a75fb50fe1de0725028eaa10a4712a839
Lay, Kuan Loon <kuan.loon.lay@intel.com>
2015-10-20 18:59:30 +0800

    iio: iio get channel and get event error checking
    Signed-off-by: Lay, Kuan Loon <kuan.loon.lay@intel.com>

--- src/iio/iio.c
+++ src/iio/iio.c
@@ -90,6 +90,9 @@ mraa_iio_get_channel_data(mraa_iio_context dev)
         }
     }
     dev->chan_num = chan_num;
+	//no need proceed if no channel found
+	if (chan_num == 0)
+		return MRAA_SUCCESS;
     mraa_iio_channel* chan;
     dev->channels = calloc(chan_num, sizeof(mraa_iio_channel));
     seekdir(dir, 0);
@@ -304,6 +307,9 @@ mraa_iio_get_event_data(mraa_iio_context dev)
             }
         }
         dev->event_num = event_num;
+		//no need proceed if no event found
+		if (event_num == 0)
+			return MRAA_SUCCESS;
         mraa_iio_event* event;
 		dev->events = calloc(event_num, sizeof(mraa_iio_event));
 		if ( dev->events == NULL)
