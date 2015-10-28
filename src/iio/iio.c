4ecfe61fe89a5e2c481637d9c576429815363cff
Lay, Kuan Loon <kuan.loon.lay@intel.com>
2015-10-28 16:29:26 +0800

    iio: read float fix
    Signed-off-by: Lay, Kuan Loon <kuan.loon.lay@intel.com>

--- src/iio/iio.c
+++ src/iio/iio.c
@@ -93,9 +93,9 @@ mraa_iio_get_channel_data(mraa_iio_context dev)
         }
     }
     dev->chan_num = chan_num;
-	//no need proceed if no channel found
-	if (chan_num == 0)
-		return MRAA_SUCCESS;
+    //no need proceed if no channel found
+    if (chan_num == 0)
+        return MRAA_SUCCESS;
     mraa_iio_channel* chan;
     dev->channels = calloc(chan_num, sizeof(mraa_iio_channel));
     seekdir(dir, 0);
@@ -205,12 +205,12 @@ mraa_iio_get_device_num_by_name(const char* name)
 mraa_result_t
 mraa_iio_read(mraa_iio_context dev, const char* attr_chan, float* data)
 {
-    char buf[64];
-    snprintf(buf, 64, IIO_SYSFS_DEVICE "%d/%s", dev->num, attr_chan);
+    char buf[128];
+    snprintf(buf, 128, IIO_SYSFS_DEVICE "%d/%s", dev->num, attr_chan);
     int fd = open(buf, O_RDONLY);
     if (fd != -1) {
-        int len = read(fd, &buf, 64);
-        *data = strtol(buf, NULL, 10);
+        int len = read(fd, &buf, 128);
+        *data = strtof(buf, NULL);
         return MRAA_SUCCESS;
     }
     return MRAA_ERROR_UNSPECIFIED;
@@ -263,7 +263,7 @@ mraa_iio_trigger_handler(void* arg)
         if (mraa_iio_wait_event(dev->fp, &data[0], &read_size) == MRAA_SUCCESS) {
             pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
             //only can process if readsize >= enabled channel's datasize
-	    for (i=0; i<(read_size/dev->datasize); i++) {
+        for (i=0; i<(read_size/dev->datasize); i++) {
                 dev->isr(&data);
             }
             pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
@@ -318,37 +318,37 @@ mraa_iio_get_event_data(mraa_iio_context dev)
             }
         }
         dev->event_num = event_num;
-		//no need proceed if no event found
-		if (event_num == 0)
-			return MRAA_SUCCESS;
+        //no need proceed if no event found
+        if (event_num == 0)
+            return MRAA_SUCCESS;
         mraa_iio_event* event;
-		dev->events = calloc(event_num, sizeof(mraa_iio_event));
-		if ( dev->events == NULL)
-		{
-			closedir(dir);
-			return MRAA_ERROR_UNSPECIFIED;
-		}
-		rewinddir(dir);
-		event_num = 0;
-		while ((ent = readdir(dir)) != NULL)
-		{
+        dev->events = calloc(event_num, sizeof(mraa_iio_event));
+        if ( dev->events == NULL)
+        {
+            closedir(dir);
+            return MRAA_ERROR_UNSPECIFIED;
+        }
+        rewinddir(dir);
+        event_num = 0;
+        while ((ent = readdir(dir)) != NULL)
+        {
             if (strcmp(ent->d_name + strlen(ent->d_name) - strlen("_en"), "_en") == 0) {
-				event = &dev->events[event_num];
+                event = &dev->events[event_num];
                 event->name = strdup(ent->d_name);
-				snprintf(buf, MAX_SIZE, IIO_SYSFS_DEVICE "%d/" IIO_EVENTS "/%s", dev->num, ent->d_name);
-				fd = open(buf, O_RDONLY);
-				if (fd > 0) {
-					if (read(fd, readbuf, 2 * sizeof(char)) != 2) {
-						break;
-					}
-					close(fd);
-				}
+                snprintf(buf, MAX_SIZE, IIO_SYSFS_DEVICE "%d/" IIO_EVENTS "/%s", dev->num, ent->d_name);
+                fd = open(buf, O_RDONLY);
+                if (fd > 0) {
+                    if (read(fd, readbuf, 2 * sizeof(char)) != 2) {
+                        break;
+                    }
+                    close(fd);
+                }
                 event->enabled = ((int) strtol(readbuf, NULL, 10));
-				//Todo, read other event info.
-				event_num++;
+                //Todo, read other event info.
+                event_num++;
             }
         }
-		closedir(dir);
+        closedir(dir);
     }
     return MRAA_SUCCESS;
 }
@@ -356,12 +356,12 @@ mraa_iio_get_event_data(mraa_iio_context dev)
 mraa_result_t
 mraa_iio_event_read(mraa_iio_context dev, const char* attribute, float* data)
 {
-    char buf[64];
-    snprintf(buf, 64, IIO_SYSFS_DEVICE "%d/" IIO_EVENTS "/%s", dev->num, attribute);
+    char buf[128];
+    snprintf(buf, 128, IIO_SYSFS_DEVICE "%d/" IIO_EVENTS "/%s", dev->num, attribute);
     int fd = open(buf, O_RDONLY);
     if (fd != -1) {
-        int len = read(fd, &buf, 64);
-        *data = strtol(buf, NULL, 10);
+        int len = read(fd, &buf, 128);
+        *data = strtof(buf, NULL);
         return MRAA_SUCCESS;
     }
     return MRAA_ERROR_UNSPECIFIED;
@@ -370,12 +370,12 @@ mraa_iio_event_read(mraa_iio_context dev, const char* attribute, float* data)
 mraa_result_t
 mraa_iio_event_write(mraa_iio_context dev, const char* attribute,  const char* data)
 {
-	int len;
+    int len;
     char buf[128];
     snprintf(buf, 128, IIO_SYSFS_DEVICE "%d/" IIO_EVENTS "/%s", dev->num, attribute);
     int fd = open(buf, O_WRONLY);
     if (fd != -1) {
-		int len = write(fd, data, ( strlen(data) +1 ));
+        int len = write(fd, data, ( strlen(data) +1 ));
         return MRAA_SUCCESS;
     }
     return MRAA_ERROR_UNSPECIFIED;
@@ -407,27 +407,27 @@ mraa_iio_event_poll(mraa_iio_context dev, struct iio_event_data* data)
 {
     char bu[MAX_SIZE];
     int ret;
-	int event_fd;
-	int fd;
+    int event_fd;
+    int fd;
 
-	sprintf(bu, IIO_SLASH_DEV "%d", dev->num);
+    sprintf(bu, IIO_SLASH_DEV "%d", dev->num);
     fd = open(bu, 0);
-	ret = ioctl(fd, IIO_GET_EVENT_FD_IOCTL, &event_fd);
-	close(fd);
+    ret = ioctl(fd, IIO_GET_EVENT_FD_IOCTL, &event_fd);
+    close(fd);
 
-	if (ret == -1 || event_fd == -1)
-		return MRAA_ERROR_UNSPECIFIED;
+    if (ret == -1 || event_fd == -1)
+        return MRAA_ERROR_UNSPECIFIED;
 
-	ret = read(event_fd, data, sizeof(struct iio_event_data));
+    ret = read(event_fd, data, sizeof(struct iio_event_data));
 
-	close(event_fd);
+    close(event_fd);
     return MRAA_SUCCESS;
 }
 
 static void*
 mraa_iio_event_handler(void* arg)
 {
-	struct iio_event_data data;
+    struct iio_event_data data;
     mraa_iio_context dev = (mraa_iio_context) arg;
 
     for (;;) {
@@ -446,7 +446,7 @@ mraa_iio_event_handler(void* arg)
 mraa_result_t
 mraa_iio_event_setup_callback(mraa_iio_context dev, void (*fptr)(struct iio_event_data* data), void* args)
 {
-	int ret;
+    int ret;
     char bu[MAX_SIZE];
     if (dev->thread_id != 0) {
         return MRAA_ERROR_NO_RESOURCES;
@@ -457,13 +457,13 @@ mraa_iio_event_setup_callback(mraa_iio_context dev, void (*fptr)(struct iio_even
     if (dev->fp == -1) {
         return MRAA_ERROR_INVALID_RESOURCE;
     }
-	ret = ioctl(dev->fp, IIO_GET_EVENT_FD_IOCTL, &dev->fp_event);
-	close(dev->fp);
+    ret = ioctl(dev->fp, IIO_GET_EVENT_FD_IOCTL, &dev->fp_event);
+    close(dev->fp);
 
-	if (ret == -1 || dev->fp_event == -1)
-	{
-		return MRAA_ERROR_UNSPECIFIED;
-	}
+    if (ret == -1 || dev->fp_event == -1)
+    {
+        return MRAA_ERROR_UNSPECIFIED;
+    }
 
     dev->isr_event = fptr;
     pthread_create(&dev->thread_id, NULL, mraa_iio_event_handler, (void*) dev);
@@ -474,13 +474,13 @@ mraa_iio_event_setup_callback(mraa_iio_context dev, void (*fptr)(struct iio_even
 mraa_result_t
 mraa_iio_event_extract_event(struct iio_event_data* event, int* chan_type, int* modifier, int* type, int* direction, int* channel, int* channel2, int* different)
 {
-	*chan_type = IIO_EVENT_CODE_EXTRACT_CHAN_TYPE(event->id);
-	*modifier= IIO_EVENT_CODE_EXTRACT_MODIFIER(event->id);
-	*type = IIO_EVENT_CODE_EXTRACT_TYPE(event->id);
-	*direction = IIO_EVENT_CODE_EXTRACT_DIR(event->id);
-	*channel = IIO_EVENT_CODE_EXTRACT_CHAN(event->id);
-	*channel2 = IIO_EVENT_CODE_EXTRACT_CHAN2(event->id);
-	*different = IIO_EVENT_CODE_EXTRACT_DIFF(event->id);
+    *chan_type = IIO_EVENT_CODE_EXTRACT_CHAN_TYPE(event->id);
+    *modifier= IIO_EVENT_CODE_EXTRACT_MODIFIER(event->id);
+    *type = IIO_EVENT_CODE_EXTRACT_TYPE(event->id);
+    *direction = IIO_EVENT_CODE_EXTRACT_DIR(event->id);
+    *channel = IIO_EVENT_CODE_EXTRACT_CHAN(event->id);
+    *channel2 = IIO_EVENT_CODE_EXTRACT_CHAN2(event->id);
+    *different = IIO_EVENT_CODE_EXTRACT_DIFF(event->id);
     return MRAA_SUCCESS;
 }
 #if 0
