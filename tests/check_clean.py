8b68f3040e8a3e9706e52636f194082dd47d75aa
Stefan Andritoiu <stefan.andritoiu@intel.com>
2016-01-19 16:37:21 +0200

    java: added sanity checks for sample mapping and bindings
    Signed-off-by: Stefan Andritoiu <stefan.andritoiu@intel.com>
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

---
+++ tests/check_clean.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import unittest as u
+import re, fnmatch, os
+
+rootDir = 'src/java'
+swigtypeStr = 'SWIGTYPE'
+
+class Clean(u.TestCase):
+
+    def test_existing_swigtype(self):
+        unclean = []
+
+        for fileName in os.listdir(rootDir):
+            if swigtypeStr in fileName:
+                unclean.append(fileName)
+
+        self.assertEqual( len(unclean), 0,
+                "\nmraa contains unclean Java bindings:\n" + \
+                "\n".join(unclean) + "\n\n")
+
+if __name__ == '__main__':
+    u.main()
