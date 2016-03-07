0d936c74ec2cbfe6b97ee63e98295d69969a62e0
Brendan Le Foll <brendan.le.foll@intel.com>
2016-03-07 14:49:42 +0000

    firmata: initial work
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

--- api/mraa/common.h
+++ api/mraa/common.h
@@ -276,7 +276,10 @@ int mraa_get_sub_platform_id(int pin_or_bus_index);
  */
 int mraa_get_sub_platform_index(int pin_or_bus_id);
 
-
+/**
+ *
+ */
+mraa_result_t mraa_add_subplatform(mraa_platform_t subplatformtype, const char* uart_dev);
 
 #ifdef __cplusplus
 }
