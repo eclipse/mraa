4c70c4a9b2cc168d4ccb1f70b4d00c1c64f3191e
Longwei Su <longwei.su@gmail.com>
2016-03-17 15:21:02 -0400

    imraa: update udev cmake configuration
    Signed-off-by: Longwei Su <lsu@ics.com>
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

---
+++ cmake/modules/FindUDEV.cmake
@@ -0,0 +1,21 @@
+# UDEV_FOUND - true if library and headers were found
+# UDEV_INCLUDE_DIRS - include directories
+# UDEV_LIBRARIES - library directories
+
+find_package (PkgConfig)
+pkg_check_modules (PC_UDEV QUIET udev)
+
+find_path (UDEV_INCLUDE_DIR libudev.h HINTS ${PC_UDEV_INCLUDEDIR}
+${PC_UDEV_INCLUDE_DIRS} PATH_SUFFIXES udev)
+
+find_library (UDEV_LIBRARY NAMES udev libudev HINTS ${PC_UDEV_LIBDIR}
+${PC_UDEV_LIBRARY_DIRS})
+
+set (UDEV_LIBRARIES ${UDEV_LIBRARY})
+set (UDEV_INCLUDE_DIRS ${UDEV_INCLUDE_DIR})
+
+include (FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args (UDEV DEFAULT_MSG UDEV_LIBRARY UDEV_INCLUDE_DIR)
+
+mark_as_advanced (UDEV_INCLUDE_DIR UDEV_LIBRARY)
