6fc2bef13f9cbe56b89b4b079acf268979aa654f
Houman Brinjcargorabi <houman.brinjcargorabi@intel.com>
2016-03-21 11:34:50 +0000

    hellomraa.c: Remove unused variables
    Signed-off-by: Houman Brinjcargorabi <houman.brinjcargorabi@intel.com>
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

--- examples/hellomraa.c
+++ examples/hellomraa.c
@@ -32,8 +32,6 @@ int
 main(int argc, char** argv)
 {
     const char* board_name = mraa_get_platform_name();
-    int i2c_bus, i, i2c_adapter;
-
     fprintf(stdout, "hello mraa\n Version: %s\n Running on %s\n", mraa_get_version(), board_name);
 
     mraa_deinit();
