a17079c4609679d767b4470f6fd9a88950fd6b64
Brendan Le Foll <brendan.le.foll@intel.com>
2016-03-21 15:19:00 +0000

    firmata: install node_modules to where cmake guessed node was
    This does now mean that modules won't go to /usr/local with the default prefix
    but may instead try to install in /usr/lib. However if using nvm they should go
    to the right place as long as NODE_ROOT_DIR is set correctly. This commit
    changes NODE_ROOT_DIR to actually refer to the root directory of node rather
    than the include dir which is what FindNodejs gets
    
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

--- src/javascript/package.json.cmake
+++ src/javascript/package.json.cmake
@@ -1,7 +1,7 @@
 {
   "name" : "mraa",
   "description": "IO library that helps you use I2c, SPI, gpio, uart, pwm, analog inputs (aio) and more on a number of platforms such as the Intel galileo, the Intel edison and others",
-  "keywords":["gpio", "edison","galileo","io", "mraajs", "spi", "i2c", "minnow", "intel"],
+  "keywords":["gpio", "edison","galileo","io", "mraajs", "spi", "i2c", "minnow", "intel", "firmata"],
   "homepage": "http://github.com/intel-iot-devkit/mraa",
   "main" : "./mraa.node",
   "engines": {
