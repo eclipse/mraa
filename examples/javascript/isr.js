01e6c4b50287ed9a901ff59ee60205ac29790a56
Alex Tereschenko <alext.mkrs@gmail.com>
2016-03-28 18:41:43 +0200

    isr.js: updated JS ISR example to avoid GPIO context being destroyed
    Closes #403.
    
    Signed-off-by: Alex Tereschenko <alext.mkrs@gmail.com>

--- examples/javascript/isr.js
+++ examples/javascript/isr.js
@@ -8,3 +8,9 @@ function h() {
 
 x = new m.Gpio(14)
 x.isr(m.EDGE_BOTH, h)
+
+setInterval(function() {
+    // It's important to refer to our GPIO context here,
+    // otherwise it will be garbage-collected
+    console.log("Waiting for an interrupt at GPIO pin " + x.getPin() + "...")
+}, 10000)
