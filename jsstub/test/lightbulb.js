05dc500a88a85f9487bb8b48e2eb5ccf101fd6a6
David Antler <david.a.antler@intel.com>
2016-05-20 16:44:39 -0700

    Add JavaScript mraaStub project
    Signed-off-by: David Antler <david.a.antler@intel.com>

---
+++ jsstub/test/lightbulb.js
@@ -0,0 +1,32 @@
+/**
+* @fileoverview Implementation of a LightBulb class abstraction
+*
+*/
+
+module.exports = (function() {
+    "use strict";
+    var m = require('../index');
+
+    /**
+     * Constructor for a new LightBulb
+     * @class LightBulb
+     *
+     * @classdesc This class abstracts access to the control and data registers
+     * on an imaginary lightbulb.
+     * @param {Object} A libmraa I2c object, initialized
+     */
+    function LightBulb (i2cInterface) {
+        var self = this;
+        self._i2cInterface = i2cInterface;
+
+        self.getBrightness = function() {
+            // Presume our brightness data is one byte at offset 4
+            return self._i2cInterface.readReg(4);
+        }
+
+        return self;
+    }
+
+    return LightBulb;
+})();
+
