7b7b11415b187e7f2aba886abf8b91dbeef25e8a
Brendan Le Foll <brendan.le.foll@intel.com>
2016-11-04 12:48:47 +0000

    imraa.c: We have 0 args as minimum so remove error check for < 0 against unsinged int
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

--- imraa/imraa.c
+++ imraa/imraa.c
@@ -118,10 +118,6 @@ parse_opt(int key, char *arg, struct argp_state *state)
             arguments->args[state->arg_num] = arg;
             break;
         case ARGP_KEY_END:
-            if (state->arg_num < 0) {
-                /* Not enough arguments. */
-                argp_usage (state);
-            }
             break;
         default:
             return ARGP_ERR_UNKNOWN;
