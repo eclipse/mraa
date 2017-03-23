62b9be4da4e41faee3aaa7a8c1d3963b7f064f44
Nicola Lunghi@Emutex <nicola.lunghi@emutex.com>
2017-03-23 16:01:58 +0000

    Update platform_checks.py
    Resolve the error on platform different than edison
    Signed-off-by: Nicola Lunghi <nicola.lunghi@emutex.com>

--- tests/platform_checks.py
+++ tests/platform_checks.py
@@ -29,6 +29,11 @@ import unittest as u
 
 @u.skipIf(m.getPlatformName() == "Unknown platform", "Not valid platform loaded")
 class PlatformChecks(u.TestCase):
+  pass
+
+
+@u.skipUnless(m.getPlatformName() == "Intel Edison", "Only for edison Board")
+class PlatformChecksEdison(u.TestCase):
   def test_mraa_check_platform_no_of_pins(self):
     pinCount = m.getPinCount()
     self.assertEqual(pinCount, 20, "Wrong number. of pins. Check platform ...")
@@ -41,7 +46,7 @@ class PlatformChecks(u.TestCase):
   def test_mraa_check_platform_ADC_resolution(self):
     self.p_ADC_res = m.adcSupportedBits()
     print("Platform ADC resolution is: " + str(self.p_ADC_res) + " bits")
-    self.assertEqual(self.p_ADC_res, 10, "Wrong ADC suported resolution. Check platform ...")
+    self.assertEqual(self.p_ADC_res, 10, "Wrong ADC supported resolution. Check platform ...")
 
 if __name__ == "__main__":
   u.main()
