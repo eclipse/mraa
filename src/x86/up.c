bce99013cd9e960231f9ee3b136d678e69d6c02f
Nicola Lunghi <nicola.lunghi@emutex.com>
2017-05-03 13:51:30 +0100

    Fixed pin name size on up board
    Signed-off-by: Nicola Lunghi <nicola.lunghi@emutex.com>

--- src/x86/up.c
+++ src/x86/up.c
@@ -31,14 +31,13 @@
 #include "x86/up.h"
 
 #define PLATFORM_NAME "UP"
-#define MAX_LENGTH 8
 
 static mraa_result_t
 mraa_up_set_pininfo(mraa_board_t* board, int mraa_index, char* name, mraa_pincapabilities_t caps, int sysfs_pin)
 {
     if (mraa_index < board->phy_pin_count) {
         mraa_pininfo_t* pin_info = &board->pins[mraa_index];
-        strncpy(pin_info->name, name, MAX_LENGTH);
+        strncpy(pin_info->name, name, MRAA_PIN_NAME_SIZE);
         pin_info->capabilities = caps;
         if (caps.gpio) {
             pin_info->gpio.pinmap = sysfs_pin;
