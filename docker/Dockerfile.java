9d98196b60d3d52eaa8592b4e18c38a47a8564e7
Nicolas Oliver <dario.n.oliver@intel.com>
2017-07-04 10:44:13 -0700

    travis: improve docker images generation for building
    Make a build heriarchy to reduce images size.
    Add Android Thing Build.
    
    Signed-off-by: Nicolas Oliver <dario.n.oliver@intel.com>

---
+++ docker/Dockerfile.java
@@ -0,0 +1,12 @@
+FROM mraa-base
+
+# Update apt-get
+RUN apt-get -y update && \
+  # Java Build Dependencies
+  apt-get -y --no-install-recommends install default-jre default-jdk
+
+# Set JAVA_HOME
+ENV JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64/
+
+CMD bash
+
