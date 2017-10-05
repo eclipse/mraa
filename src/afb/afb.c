bac7b3e1019a8f26ef0b81629055831d7db208a9
Brendan Le Foll <brendan.le.foll@intel.com>
2017-10-05 17:42:18 +0200

    afb: attempt at synchronous AFB call
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

--- src/afb/afb.c
+++ src/afb/afb.c
@@ -56,11 +56,11 @@ static struct afb_wsj1_itf itf = {
 };
 
 /* global variables */
-static struct afb_wsj1 *wsj1;
+static struct afb_wsj1* wsj1;
 
 /* makes a call to AFB binding */
 static void
-afb_call(const char *api, const char *verb, const char *object)
+_afb_call(const char* api, const char* verb, const char* object)
 {
     char *key;
     int rc;
@@ -72,10 +72,51 @@ afb_call(const char *api, const char *verb, const char *object)
     }
 }
 
+struct _afb_lock_msg {
+    pthread_spinlock_t* lock;
+    struct afb_wsj1_msg* msg;
+};
+
+static void
+_afb_block(void* closure, struct afb_wsj1_msg* msg)
+{
+    struct _afb_lock_msg* lockmsg = (struct _afb_lock_msg*) closure;
+    lockmsg->msg = msg;
+    pthread_spin_unlock(lockmsg->lock);
+}
+
+static mraa_result_t
+_afb_call_block_res(const char* api, const char* verb, const char* object)
+{
+    int rc;
+
+    struct _afb_lock_msg lockmsg;
+    pthread_spinlock_t pspinlock;
+    lockmsg.lock = &pspinlock;
+    int ret = pthread_spin_init(lockmsg.lock, PTHREAD_PROCESS_SHARED);
+    if (ret != 0) {
+        syslog(LOG_ERR, "afb: could not init locking");
+        return MRAA_ERROR_NO_RESOURCES;
+    }
+
+    /* send the request */
+    pthread_spin_lock(lockmsg.lock);
+    rc = afb_wsj1_call_s(wsj1, api, verb, object, _afb_block, &lockmsg);
+    if (rc < 0) {
+        fprintf(stderr, "calling %s/%s(%s) failed: %m\n", api, verb, object);
+	pthread_spin_unlock(lockmsg.lock);
+	return MRAA_ERROR_INVALID_RESOURCE;
+    }
+    pthread_spin_lock(lockmsg.lock);
+    pthread_spin_destroy(lockmsg.lock);
+
+    return MRAA_SUCCESS;
+}
+
 mraa_result_t
 mraa_afb_i2c_init_bus_replace(mraa_i2c_context dev)
 {
-    afb_call("mraa", "dev-init", "['mraa_i2c_init', 0]");
+    _afb_call("mraa", "dev-init", "['mraa_i2c_init', 0]");
     return MRAA_SUCCESS;
 }
 
@@ -100,9 +141,10 @@ mraa_afb_i2c_address_replace(mraa_i2c_context dev, uint8_t addr)
     char* command;
     command = calloc(sizeof(char), 255);
     snprintf(command, 255, "['mraa_i2c_address', 0, %d]", addr);
-    afb_call("mraa", "command", command);
+//    _afb_call("mraa", "command", command);
+    return _afb_call_block_res("mraa", "command", command);
 
-    return MRAA_SUCCESS;
+//    return MRAA_SUCCESS;
 }
 
 int
@@ -118,19 +160,19 @@ mraa_afb_i2c_read_byte_replace(mraa_i2c_context dev)
 }
 
 int
-mraa_afb_i2c_read_byte_data_replace(mraa_i2c_context dev, uint8_t command)
+mraa_afb_i2c_read_byte_data_replace(mraa_i2c_context dev, uint8_t reg)
 {
     return -1;
 }
 
 int
-mraa_afb_i2c_read_bytes_data_replace(mraa_i2c_context dev, uint8_t command, uint8_t* data, int length)
+mraa_afb_i2c_read_bytes_data_replace(mraa_i2c_context dev, uint8_t reg, uint8_t* data, int length)
 {
     return -1;
 }
 
 int
-mraa_afb_i2c_read_word_data_replace(mraa_i2c_context dev, uint8_t command)
+mraa_afb_i2c_read_word_data_replace(mraa_i2c_context dev, uint8_t reg)
 {
     return -1;
 }
@@ -141,7 +183,7 @@ mraa_afb_i2c_write_replace(mraa_i2c_context dev, const uint8_t* data, int length
     char* command;
     command = calloc(sizeof(char), 255);
     snprintf(command, 255, "['mraa_i2c_write', 0, %s, %d]", data, length);
-    afb_call("mraa", "command", command);
+    _afb_call("mraa", "command", command);
     return MRAA_SUCCESS;
 }
 
@@ -151,7 +193,7 @@ mraa_afb_i2c_write_byte_replace(mraa_i2c_context dev, const uint8_t data)
     char* command;
     command = calloc(sizeof(char), 255);
     snprintf(command, 255, "['mraa_i2c_write_byte', 0, %d]", data);
-    afb_call("mraa", "command", command);
+    _afb_call("mraa", "command", command);
     return MRAA_SUCCESS;
 }
 
@@ -161,14 +203,18 @@ mraa_afb_i2c_write_byte_data_replace(mraa_i2c_context dev, const uint8_t data, c
     char* command;
     command = calloc(sizeof(char), 255);
     snprintf(command, 255, "['mraa_i2c_write_byte_data', 0, %d, %d]", data, reg);
-    afb_call("mraa", "command", command);
+    _afb_call("mraa", "command", command);
     return MRAA_SUCCESS;
 }
 
 mraa_result_t
 mraa_afb_i2c_write_word_data_replace(mraa_i2c_context dev, const uint16_t data, const uint8_t reg)
 {
-    return MRAA_ERROR_UNSPECIFIED;
+    char* command;
+    command = calloc(sizeof(char), 255);
+    snprintf(command, 255, "['mraa_i2c_write_word_data', 0, %d, %d]", data, reg);
+    _afb_call("mraa", "command", command);
+    return MRAA_SUCCESS;
 }
 
 mraa_board_t*
