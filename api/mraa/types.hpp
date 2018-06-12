4c4845b101c32384bb35fdc3029fa7415705dcde
Mihai Tudor Panu <mihai.tudor.panu@intel.com>
2018-06-11 22:52:37 -0700

    types.hpp: add missing mediatek boards to platform enum
    Signed-off-by: Mihai Tudor Panu <mihai.tudor.panu@intel.com>

--- api/mraa/types.hpp
+++ api/mraa/types.hpp
@@ -55,6 +55,8 @@ typedef enum {
     PHYBOARD_WEGA = 14,        /**< The phyBOARD-Wega */
     DE_NANO_SOC = 15,          /**< Terasic DE-Nano-SoC Board */
     INTEL_UP2 = 16,            /**< The UP^2 Board */
+    MTK_LINKIT = 17,           /**< Mediatek MT7688 based Linkit boards */
+    MTK_OMEGA2 = 18,           /**< MT7688 based Onion Omega2 board */
     IEI_TANK = 19,             /**< IEI Tank System*/
 
     FTDI_FT4222 = 256,         /**< FTDI FT4222 USB to i2c bridge */
