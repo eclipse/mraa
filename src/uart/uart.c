3b2f80b6e29656b4a17d5da6dfd2b10e67863314
Benxi Liu <bxliu@linux.alibaba.com>
2019-09-06 17:56:12 +0800

    uart: fix xonxoff value in mraa_uart_settings()
    the value of xonxoff(IXON|IXOFF) resides in the c_iflag field in
    struct termios, not c_cflag.
    
    Signed-off-by: Benxi Liu <bxliu@linux.alibaba.com>

--- src/uart/uart.c
+++ src/uart/uart.c
@@ -419,7 +419,7 @@ mraa_uart_settings(int index, const char **devpath, const char **name, int* baud
        }
 
        if (xonxoff != NULL) {
-           *xonxoff = term.c_cflag & (IXON|IXOFF);
+           *xonxoff = term.c_iflag & (IXON|IXOFF);
        }
 
        close(fd);
