4284c04fb19b0dbaad99b446b3394e784fa55e8b
Chuckduey <cduey@msn.com>
2020-06-21 11:32:16 -0600

    Added Pi 4 8GB to Pi 4 section
    Signed-off-by: Chuckduey <cduey@msn.com>

--- src/arm/raspberry_pi.c
+++ src/arm/raspberry_pi.c
@@ -506,7 +506,8 @@ mraa_raspberry_pi()
                     block_size = BCM2837_BLOCK_SIZE;
                 } else if (strstr(line, "a03111") || 
                     strstr(line, "b03111") || strstr(line, "b03112") ||
-                    strstr(line, "c03111") || strstr(line, "c03112")) {
+                    strstr(line, "c03111") || strstr(line, "c03112") ||
+                    strstr(line, "d03114")) {
                     b->platform_name = PLATFORM_NAME_RASPBERRY_PI4_B;
                     platform_detected = PLATFORM_RASPBERRY_PI4_B;
                     b->phy_pin_count = MRAA_RASPBERRY_PI4_B_PINCOUNT;
