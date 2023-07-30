42ad029980a556a26a017e51a3514e1a63c2b3c2
paul.chang <paul.chang@mrshim.de>
2023-07-30 12:58:48 +0200

    rockpis: Modified rockpi4 to be compatible with Rock Pi S

--- include/arm/rockpi4.h
+++ include/arm/rockpi4.h
@@ -1,5 +1,6 @@
 /*
  * Author: Brian <brian@vamrs.com>
+ * Author: Paul <paul.chang@mrshim.de>
  * Copyright (c) 2019 Vamrs Corporation.
  *
  * SPDX-License-Identifier: MIT
@@ -13,13 +14,34 @@ extern "C" {
 
 #include "mraa_internal.h"
 
-#define MRAA_ROCKPI4_GPIO_COUNT 27
+/*
+ * Currently 2 product versions are supported: Rock Pi 4 and Rock Pi S
+ */
+#define ROCKPI_NUM_SUPPORTED_HW 2
+
+/*
+ * Defines for Rock Pi 4 boards.
+ */
+#define MRAA_ROCKPI4_INDEX      0
 #define MRAA_ROCKPI4_I2C_COUNT  3
 #define MRAA_ROCKPI4_SPI_COUNT  2
 #define MRAA_ROCKPI4_UART_COUNT 2
 #define MRAA_ROCKPI4_PWM_COUNT  2
 #define MRAA_ROCKPI4_AIO_COUNT  1
 #define MRAA_ROCKPI4_PIN_COUNT  40
+
+/*
+ * Defines for Rock Pi S boards (V11, V12, 13)
+ * Since there hardware board versions cannot be distinguished
+ * programatically,the lowest counts are be used.
+ */
+#define MRAA_ROCKPIS_INDEX      1
+#define MRAA_ROCKPIS_I2C_COUNT  3
+#define MRAA_ROCKPIS_SPI_COUNT  1
+#define MRAA_ROCKPIS_UART_COUNT 2
+#define MRAA_ROCKPIS_PWM_COUNT  2
+#define MRAA_ROCKPIS_AIO_COUNT  1
+#define MRAA_ROCKPIS_PIN_COUNT  52
 
 mraa_board_t *
         mraa_rockpi4();
--- src/arm/rockpi4.c
+++ src/arm/rockpi4.c
@@ -1,5 +1,6 @@
 /*
  * Author: Brian <brian@vamrs.com>
+ * Author: Paul <paul.chang@mrshim.de>
  * Copyright (c) 2019 Vamrs Corporation.
  *
  * SPDX-License-Identifier: MIT
@@ -15,6 +16,7 @@
 #include "common.h"
 
 #define DT_BASE "/proc/device-tree"
+
 /*
 * "Radxa ROCK Pi 4" is the model name on stock 5.x kernels
 * "ROCK PI 4A", "ROCK PI 4B" and "ROCK PI 4C" is used on Radxa 4.4 kernel
@@ -23,12 +25,40 @@
 #define PLATFORM_NAME_ROCK_PI4 "ROCK Pi 4"
 #define PLATFORM_NAME_ROCK_PI4_2 "ROCK PI 4"
 #define PLATFORM_NAME_ROCK_PI4_3 "ROCK 4"
-#define MAX_SIZE 64
 
-const char* rockpi4_serialdev[MRAA_ROCKPI4_UART_COUNT] = { "/dev/ttyS2", "/dev/ttyS4" };
+#define PLATFORM_NAME_ROCK_PIS "ROCK Pi S"
+
+#define MAX_SIZE 64
+#define MAX_PERIPH 4
+
+typedef struct
+{
+    int serialdev_index[MAX_PERIPH];
+    int i2cbus_index[MAX_PERIPH];
+    int spibus_index[MAX_PERIPH];
+    int pwmdev_index[MAX_PERIPH];
+} PeripheralIndex;
+
+PeripheralIndex rockpi_index[ROCKPI_NUM_SUPPORTED_HW] = {
+    // Peripheral indices for Rock Pi 4
+    {
+        .serialdev_index = { 2, 4 },
+        .i2cbus_index = { 7, 2, 6 },
+        .spibus_index = { 1, 2 },
+        .pwmdev_index = { 0, 1 }
+    },
+
+    // Peripheral indices for Rock Pi S
+    {
+        .serialdev_index = { 0, 1, 2 },
+        .i2cbus_index = { 1, 0, 3 },
+        .spibus_index = { 2 },
+        .pwmdev_index = { 2, 3 }
+    }
+};
 
 void
-mraa_rockpi4_pininfo(mraa_board_t* board, int index, int sysfs_pin, mraa_pincapabilities_t pincapabilities_t, char* fmt, ...)
+mraa_rockpi_pininfo(mraa_board_t* board, int index, int sysfs_pin, mraa_pincapabilities_t pincapabilities_t, char* fmt, ...)
 {
     va_list arg_ptr;
     if (index > board->phy_pin_count)
@@ -50,9 +80,118 @@ mraa_rockpi4_pininfo(mraa_board_t* board, int index, int sysfs_pin, mraa_pincapa
     pininfo->gpio.mux_total = 0;
 }
 
+void
+rockpi4_pins(mraa_board_t* b)
+{
+    mraa_rockpi_pininfo(b, 0,   -1, (mraa_pincapabilities_t){0,0,0,0,0,0,0,0}, "INVALID");
+    mraa_rockpi_pininfo(b, 1,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "3V3");
+    mraa_rockpi_pininfo(b, 2,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "5V");
+    mraa_rockpi_pininfo(b, 3,   71, (mraa_pincapabilities_t){1,1,0,0,0,1,0,0}, "SDA7");
+    mraa_rockpi_pininfo(b, 4,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "5V");
+    mraa_rockpi_pininfo(b, 5,   72, (mraa_pincapabilities_t){1,1,0,0,0,1,0,0}, "SCL7");
+    mraa_rockpi_pininfo(b, 6,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
+    mraa_rockpi_pininfo(b, 7,   75, (mraa_pincapabilities_t){1,1,0,0,1,0,0,0}, "SPI2_CLK");
+    mraa_rockpi_pininfo(b, 8,  148, (mraa_pincapabilities_t){1,1,0,0,0,0,0,1}, "TXD2");
+    mraa_rockpi_pininfo(b, 9,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
+    mraa_rockpi_pininfo(b, 10, 147, (mraa_pincapabilities_t){1,1,0,0,0,0,0,1}, "RXD2");
+    mraa_rockpi_pininfo(b, 11, 146, (mraa_pincapabilities_t){1,1,1,0,0,0,0,0}, "PWM0");
+    mraa_rockpi_pininfo(b, 12, 131, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_A3");
+    mraa_rockpi_pininfo(b, 13, 150, (mraa_pincapabilities_t){1,1,1,0,0,0,0,0}, "PWM1");
+    mraa_rockpi_pininfo(b, 14,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
+    mraa_rockpi_pininfo(b, 15, 149, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_C5");
+    mraa_rockpi_pininfo(b, 16, 154, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_D2");
+    mraa_rockpi_pininfo(b, 17,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "3V3");
+    mraa_rockpi_pininfo(b, 18, 156, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_D4");
+    mraa_rockpi_pininfo(b, 19,  40, (mraa_pincapabilities_t){1,1,0,0,1,0,0,1}, "SPI1TX,TXD4");
+    mraa_rockpi_pininfo(b, 20,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
+    mraa_rockpi_pininfo(b, 21,  39, (mraa_pincapabilities_t){1,1,0,0,1,0,0,1}, "SPI1RX,RXD4");
+    mraa_rockpi_pininfo(b, 22, 157, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_D5");
+    mraa_rockpi_pininfo(b, 23,  41, (mraa_pincapabilities_t){1,1,0,0,1,0,0,0}, "SPI1CLK");
+    mraa_rockpi_pininfo(b, 24,  42, (mraa_pincapabilities_t){1,1,0,0,1,0,0,0}, "SPI1CS");
+    mraa_rockpi_pininfo(b, 25,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
+    mraa_rockpi_pininfo(b, 26,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,1,0}, "ADC_IN0");
+    mraa_rockpi_pininfo(b, 27,  64, (mraa_pincapabilities_t){1,1,0,0,0,1,0,0}, "SDA2");
+    mraa_rockpi_pininfo(b, 28,  65, (mraa_pincapabilities_t){1,1,0,0,0,1,0,0}, "SCL2");
+    mraa_rockpi_pininfo(b, 29,  74, (mraa_pincapabilities_t){1,1,0,0,1,1,0,0}, "SCL6,SPI2RX");
+    mraa_rockpi_pininfo(b, 30,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
+    mraa_rockpi_pininfo(b, 31,  73, (mraa_pincapabilities_t){1,1,0,0,1,1,0,0}, "SDA6,SPI2TX");
+    mraa_rockpi_pininfo(b, 32, 112, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO3_C0");
+    mraa_rockpi_pininfo(b, 33,  76, (mraa_pincapabilities_t){1,1,0,0,1,0,0,0}, "SPI2CS");
+    mraa_rockpi_pininfo(b, 34,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
+    mraa_rockpi_pininfo(b, 35, 133, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_A5");
+    mraa_rockpi_pininfo(b, 36, 132, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_A4");
+    mraa_rockpi_pininfo(b, 37, 158, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_D6");
+    mraa_rockpi_pininfo(b, 38, 134, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_A6");
+    mraa_rockpi_pininfo(b, 39,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
+    mraa_rockpi_pininfo(b, 40, 135, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_A7");
+}
+
+void
+rockpiS_pins(mraa_board_t* b)
+{
+    // GPIO pin names correspond to boards V12 and V13
+    mraa_rockpi_pininfo(b, 0,   -1, (mraa_pincapabilities_t){0,0,0,0,0,0,0,0}, "INVALID");
+    mraa_rockpi_pininfo(b, 1,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "3V3");
+    mraa_rockpi_pininfo(b, 2,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "5V");
+    mraa_rockpi_pininfo(b, 3,   11, (mraa_pincapabilities_t){1,0,0,0,0,1,0,0}, "SDA1");
+    mraa_rockpi_pininfo(b, 4,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "5V");
+    mraa_rockpi_pininfo(b, 5,   12, (mraa_pincapabilities_t){1,1,0,0,0,1,0,0}, "SCL1");
+    mraa_rockpi_pininfo(b, 6,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
+    mraa_rockpi_pininfo(b, 7,   68, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO2_A4");
+    mraa_rockpi_pininfo(b, 8,   65, (mraa_pincapabilities_t){1,1,0,0,0,0,0,1}, "TXD0");
+    mraa_rockpi_pininfo(b, 9,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
+    mraa_rockpi_pininfo(b, 10,  64, (mraa_pincapabilities_t){1,1,0,0,0,0,0,1}, "RXD0");
+    mraa_rockpi_pininfo(b, 11,  15, (mraa_pincapabilities_t){1,1,1,0,0,1,0,0}, "PWM2");
+    mraa_rockpi_pininfo(b, 12,  69, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO2_A5");
+    mraa_rockpi_pininfo(b, 13,  16, (mraa_pincapabilities_t){1,1,1,0,0,1,0,0}, "PWM3");
+    mraa_rockpi_pininfo(b, 14,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
+    mraa_rockpi_pininfo(b, 15,  17, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO0_C1");
+    mraa_rockpi_pininfo(b, 16,  74, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO2_B2");
+    mraa_rockpi_pininfo(b, 17,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "3V3");
+    mraa_rockpi_pininfo(b, 18,  73, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO2_B1");
+    mraa_rockpi_pininfo(b, 19,  55, (mraa_pincapabilities_t){1,1,0,0,1,0,0,1}, "SPI2TX,TXD2");
+    mraa_rockpi_pininfo(b, 20,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
+    mraa_rockpi_pininfo(b, 21,  54, (mraa_pincapabilities_t){1,1,0,0,1,0,0,1}, "SPI2RX,RXD2");
+    mraa_rockpi_pininfo(b, 22,  71, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO2_A7");
+    mraa_rockpi_pininfo(b, 23,  56, (mraa_pincapabilities_t){1,1,0,0,1,1,0,1}, "CK2,SDA0,RX1");
+    mraa_rockpi_pininfo(b, 24,  57, (mraa_pincapabilities_t){1,1,0,0,1,1,0,1}, "CS2,SCL0,TX1");
+    mraa_rockpi_pininfo(b, 25,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
+    mraa_rockpi_pininfo(b, 26,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,1,0}, "ADC_IN0");
+
+    // Only use GPIO pins that are valid for all board versions: V11 to V13
+    mraa_rockpi_pininfo(b, 27,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
+    mraa_rockpi_pininfo(b, 28,  77, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO2_B5");
+    mraa_rockpi_pininfo(b, 29,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "ADC_KEY_IN1");
+    mraa_rockpi_pininfo(b, 30,  78, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO2_B6");
+    mraa_rockpi_pininfo(b, 31,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MICBIAS2");
+    mraa_rockpi_pininfo(b, 32,  79, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO2_B7");
+    mraa_rockpi_pininfo(b, 33,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MICBIAS1");
+    mraa_rockpi_pininfo(b, 34,  80, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO2_C0");
+    mraa_rockpi_pininfo(b, 35,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MICN8");
+    mraa_rockpi_pininfo(b, 36,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MCIP8");
+    mraa_rockpi_pininfo(b, 37,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MICN7");
+    mraa_rockpi_pininfo(b, 38,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MCIP7");
+    mraa_rockpi_pininfo(b, 39,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MICN6");
+    mraa_rockpi_pininfo(b, 40,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MCIP6");
+    mraa_rockpi_pininfo(b, 41,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MICN5");
+    mraa_rockpi_pininfo(b, 42,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MCIP5");
+    mraa_rockpi_pininfo(b, 43,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MICN4");
+    mraa_rockpi_pininfo(b, 44,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MCIP4");
+    mraa_rockpi_pininfo(b, 45,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MICN3");
+    mraa_rockpi_pininfo(b, 46,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MCIP3");
+    mraa_rockpi_pininfo(b, 47,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MICN2");
+    mraa_rockpi_pininfo(b, 48,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MCIP2");
+    mraa_rockpi_pininfo(b, 49,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MICN1");
+    mraa_rockpi_pininfo(b, 50,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "MCIP1");
+    mraa_rockpi_pininfo(b, 51,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "LINEOUT_R");
+    mraa_rockpi_pininfo(b, 52,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "LINEOUT_L");
+}
+
 mraa_board_t*
 mraa_rockpi4()
 {
+    int i, model_index;
+    char *serialdev_path;
     mraa_board_t* b = (mraa_board_t*) calloc(1, sizeof(mraa_board_t));
     if (b == NULL) {
         return NULL;
@@ -66,7 +205,6 @@ mraa_rockpi4()
 
     // pin mux for buses are setup by default by kernel so tell mraa to ignore them
     b->no_bus_mux = 1;
-    b->phy_pin_count = MRAA_ROCKPI4_PIN_COUNT + 1;
 
     if (mraa_file_exist(DT_BASE "/model")) {
         // We are on a modern kernel, great!!!!
@@ -74,34 +212,52 @@ mraa_rockpi4()
             mraa_file_contains(DT_BASE "/model", PLATFORM_NAME_ROCK_PI4_2) ||
             mraa_file_contains(DT_BASE "/model", PLATFORM_NAME_ROCK_PI4_3)
             ) {
+            model_index = MRAA_ROCKPI4_INDEX;
+            b->phy_pin_count = MRAA_ROCKPI4_PIN_COUNT + 1;
             b->platform_name = PLATFORM_NAME_ROCK_PI4;
-            b->uart_dev[0].device_path = (char*) rockpi4_serialdev[0];
-            b->uart_dev[1].device_path = (char*) rockpi4_serialdev[1];
+            b->uart_dev_count = MRAA_ROCKPI4_UART_COUNT;
+            b->spi_bus_count = MRAA_ROCKPI4_SPI_COUNT;
+            b->pwm_dev_count = MRAA_ROCKPI4_PWM_COUNT;
+            b->aio_count = MRAA_ROCKPI4_AIO_COUNT;
+
+            if (strncmp(b->platform_name, PLATFORM_NAME_ROCK_PI4, MAX_SIZE) == 0) {
+                b->i2c_bus_count = MRAA_ROCKPI4_I2C_COUNT;
+            }
+
+        // Rock Pi S
+        } else if (mraa_file_contains(DT_BASE "/model", PLATFORM_NAME_ROCK_PIS)) {
+            model_index = MRAA_ROCKPIS_INDEX;
+            b->phy_pin_count = MRAA_ROCKPIS_PIN_COUNT + 1;
+            b->platform_name = PLATFORM_NAME_ROCK_PIS;
+            b->uart_dev_count = MRAA_ROCKPIS_UART_COUNT;
+            b->spi_bus_count = MRAA_ROCKPIS_SPI_COUNT;
+            b->pwm_dev_count = MRAA_ROCKPIS_PWM_COUNT;
+            b->aio_count = MRAA_ROCKPIS_AIO_COUNT;
+            b->i2c_bus_count = MRAA_ROCKPIS_I2C_COUNT;
         }
     }
 
     // UART
-    b->uart_dev_count = MRAA_ROCKPI4_UART_COUNT;
     b->def_uart_dev = 0;
-    b->uart_dev[0].index = 2;
-    b->uart_dev[1].index = 4;
+    for (i = 0; i < b->uart_dev_count; i++) {
+        b->uart_dev[i].index = rockpi_index[model_index].serialdev_index[i];
+        sprintf(serialdev_path, "/dev/ttyS%i", b->uart_dev[i].index);
+        b->uart_dev[i].device_path = serialdev_path;
+    }
 
     // I2C
-    if (strncmp(b->platform_name, PLATFORM_NAME_ROCK_PI4, MAX_SIZE) == 0) {
-        b->i2c_bus_count = MRAA_ROCKPI4_I2C_COUNT;
-        b->def_i2c_bus = 0;
-        b->i2c_bus[0].bus_id = 7;
-        b->i2c_bus[1].bus_id = 2;
-        b->i2c_bus[2].bus_id = 6;
+    b->def_i2c_bus = 0;
+    for (i = 0; i < b->spi_bus_count; i++) {
+        b->i2c_bus[i].bus_id = rockpi_index[model_index].i2cbus_index[i];
     }
 
     // SPI
-    b->spi_bus_count = MRAA_ROCKPI4_SPI_COUNT;
     b->def_spi_bus = 0;
-    b->spi_bus[0].bus_id = 1;
-    b->spi_bus[1].bus_id = 2;
+    for (i = 0; i < b->spi_bus_count; i++) {
+        b->spi_bus[i].bus_id = rockpi_index[model_index].spibus_index[i];
+    }
 
-    b->pwm_dev_count = MRAA_ROCKPI4_PWM_COUNT;
+    // PWM
     b->pwm_default_period = 500;
     b->pwm_max_period = 2147483;
     b->pwm_min_period = 1;
@@ -113,61 +269,32 @@ mraa_rockpi4()
         return NULL;
     }
 
-    b->pins[11].pwm.parent_id = 0;
+    b->pins[11].pwm.parent_id = rockpi_index[model_index].pwmdev_index[0];
     b->pins[11].pwm.mux_total = 0;
     b->pins[11].pwm.pinmap = 0;
-    b->pins[13].pwm.parent_id = 1;
+    b->pins[13].pwm.parent_id = rockpi_index[model_index].pwmdev_index[1];
     b->pins[13].pwm.mux_total = 0;
     b->pins[13].pwm.pinmap = 0;
 
-    b->aio_count = MRAA_ROCKPI4_AIO_COUNT;
+    // ADC
     b->adc_raw = 10;
     b->adc_supported = 10;
     b->aio_dev[0].pin = 26;
     b->aio_non_seq = 1;
     b->chardev_capable = 1;
 
-    mraa_rockpi4_pininfo(b, 0,   -1, (mraa_pincapabilities_t){0,0,0,0,0,0,0,0}, "INVALID");
-    mraa_rockpi4_pininfo(b, 1,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "3V3");
-    mraa_rockpi4_pininfo(b, 2,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "5V");
-    mraa_rockpi4_pininfo(b, 3,   71, (mraa_pincapabilities_t){1,1,0,0,0,1,0,0}, "SDA7");
-    mraa_rockpi4_pininfo(b, 4,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "5V");
-    mraa_rockpi4_pininfo(b, 5,   72, (mraa_pincapabilities_t){1,1,0,0,0,1,0,0}, "SCL7");
-    mraa_rockpi4_pininfo(b, 6,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
-    mraa_rockpi4_pininfo(b, 7,   75, (mraa_pincapabilities_t){1,1,0,0,1,0,0,0}, "SPI2_CLK");
-    mraa_rockpi4_pininfo(b, 8,  148, (mraa_pincapabilities_t){1,1,0,0,0,0,0,1}, "TXD2");
-    mraa_rockpi4_pininfo(b, 9,   -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
-    mraa_rockpi4_pininfo(b, 10, 147, (mraa_pincapabilities_t){1,1,0,0,0,0,0,1}, "RXD2");
-    mraa_rockpi4_pininfo(b, 11, 146, (mraa_pincapabilities_t){1,1,1,0,0,0,0,0}, "PWM0");
-    mraa_rockpi4_pininfo(b, 12, 131, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_A3");
-    mraa_rockpi4_pininfo(b, 13, 150, (mraa_pincapabilities_t){1,1,1,0,0,0,0,0}, "PWM1");
-    mraa_rockpi4_pininfo(b, 14,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
-    mraa_rockpi4_pininfo(b, 15, 149, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_C5");
-    mraa_rockpi4_pininfo(b, 16, 154, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_D2");
-    mraa_rockpi4_pininfo(b, 17,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "3V3");
-    mraa_rockpi4_pininfo(b, 18, 156, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_D4");
-    mraa_rockpi4_pininfo(b, 19,  40, (mraa_pincapabilities_t){1,1,0,0,1,0,0,1}, "SPI1TX,TXD4");
-    mraa_rockpi4_pininfo(b, 20,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
-    mraa_rockpi4_pininfo(b, 21,  39, (mraa_pincapabilities_t){1,1,0,0,1,0,0,1}, "SPI1RX,RXD4");
-    mraa_rockpi4_pininfo(b, 22, 157, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_D5");
-    mraa_rockpi4_pininfo(b, 23,  41, (mraa_pincapabilities_t){1,1,0,0,1,0,0,0}, "SPI1CLK");
-    mraa_rockpi4_pininfo(b, 24,  42, (mraa_pincapabilities_t){1,1,0,0,1,0,0,0}, "SPI1CS");
-    mraa_rockpi4_pininfo(b, 25,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
-    mraa_rockpi4_pininfo(b, 26,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,1,0}, "ADC_IN0");
-    mraa_rockpi4_pininfo(b, 27,  64, (mraa_pincapabilities_t){1,1,0,0,0,1,0,0}, "SDA2");
-    mraa_rockpi4_pininfo(b, 28,  65, (mraa_pincapabilities_t){1,1,0,0,0,1,0,0}, "SCL2");
-    mraa_rockpi4_pininfo(b, 29,  74, (mraa_pincapabilities_t){1,1,0,0,1,1,0,0}, "SCL6,SPI2RX");
-    mraa_rockpi4_pininfo(b, 30,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
-    mraa_rockpi4_pininfo(b, 31,  73, (mraa_pincapabilities_t){1,1,0,0,1,1,0,0}, "SDA6,SPI2TX");
-    mraa_rockpi4_pininfo(b, 32, 112, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO3_C0");
-    mraa_rockpi4_pininfo(b, 33,  76, (mraa_pincapabilities_t){1,1,0,0,1,0,0,0}, "SPI2CS");
-    mraa_rockpi4_pininfo(b, 34,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
-    mraa_rockpi4_pininfo(b, 35, 133, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_A5");
-    mraa_rockpi4_pininfo(b, 36, 132, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_A4");
-    mraa_rockpi4_pininfo(b, 37, 158, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_D6");
-    mraa_rockpi4_pininfo(b, 38, 134, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_A6");
-    mraa_rockpi4_pininfo(b, 39,  -1, (mraa_pincapabilities_t){1,0,0,0,0,0,0,0}, "GND");
-    mraa_rockpi4_pininfo(b, 40, 135, (mraa_pincapabilities_t){1,1,0,0,0,0,0,0}, "GPIO4_A7");
+    // Define pin mappings
+    switch (model_index) {
+        case MRAA_ROCKPI4_INDEX:
+            rockpi4_pins(b);
+            break;
+
+        case MRAA_ROCKPIS_INDEX:
+            rockpiS_pins(b);
+            break;
+    }
 
     return b;
 }
+
+
