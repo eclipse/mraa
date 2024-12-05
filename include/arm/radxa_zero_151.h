910a8968787deb231dfebb846fcac3aed9d37df6
Sergey Ko <u306060@gmail.com>
2024-12-05 16:54:06 -0600

    radxa zero (rev. 1.51) support

---
+++ include/arm/radxa_zero_151.h
@@ -0,0 +1,30 @@
+/*
+ * Author: Sergey Ko <u306060@gmail.com>
+ * Copyright (c) Radxa Limited.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "mraa_internal.h"
+
+#define MRAA_RADXA_ZERO_151_GPIO_COUNT 23
+#define MRAA_RADXA_ZERO_151_I2C_COUNT  2
+#define MRAA_RADXA_ZERO_151_SPI_COUNT  2
+#define MRAA_RADXA_ZERO_151_UART_COUNT 3
+#define MRAA_RADXA_ZERO_151_PWM_COUNT  2
+#define MRAA_RADXA_ZERO_151_AIO_COUNT  0
+#define MRAA_RADXA_ZERO_151_PIN_COUNT  40
+#define PLATFORM_NAME_RADXA_ZERO   "Radxa Zero"
+
+mraa_board_t *
+    mraa_radxa_zero();
+
+#ifdef __cplusplus
+}
+#endif
